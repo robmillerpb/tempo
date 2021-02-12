541a947e854de7784cb328a7b2f48bb7af1dda8b
robmillerpb <73562020+robmillerpb@users.noreply.github.com>
2021-02-12 15:48:09 +1100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Tempo`,
     author: {
-      name: `Kyle Mathews`,
+      name: `PB Design`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `pbdesign`,
     },
   },
   plugins: [
